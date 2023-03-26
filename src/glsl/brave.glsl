194c61a59e0e7d793c74c633510af2256d6644a3
Philip Pavo <pavophilip@gmail.com>
2023-03-26 21:21:48 +0200

    shader gallery

--- src/glsl/brave.glsl
+++ src/glsl/brave.glsl
@@ -2,7 +2,15 @@
 precision mediump float;
 #endif
 
-void main(){
+uniform vec2 u_resolution;
 
-    gl_FragColor = vec4(0., 0., 0., 1.);
+void main(){
+    vec2 st = gl_FragCoord.xy / u_resolution;
+
+    vec3 yellow = vec3(1., 0.843, 0.);
+    vec3 blue = vec3(0., 0.341, 0.718);
+
+    vec3 color = mix(yellow, blue, step(0.5, st.y));
+
+    gl_FragColor = vec4(color, 1.);
 }
