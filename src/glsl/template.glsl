a78cadbf8d89ac93c4219912d2a0e24028782c39
Philip Pavo <pavophilip@gmail.com>
2023-03-26 23:10:48 +0200

    template

--- src/glsl/template.glsl
+++ src/glsl/template.glsl
@@ -7,7 +7,7 @@ uniform vec2 u_resolution;
 void main(){
     vec2 st = gl_FragCoord.xy / u_resolution;
 
-    vec3 color = vec3(1., 1., 1.);
+    vec3 color = vec3(0.5, 0.5, 0.5);
 
     gl_FragColor = vec4(color, 1.);
 }
