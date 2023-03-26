56b1d4ee1e31d560b9072f4b94457836007426c9
Philip Pavo <pavophilip@gmail.com>
2023-03-26 22:02:01 +0200

    shader gallery

--- src/components/ShaderGalleryItem.js
+++ src/components/ShaderGalleryItem.js
@@ -14,7 +14,7 @@ const GalleryItemContainer = styled.div`
 const ShaderGalleryItem = ({ shader }) => {
   return (
     <GalleryItemContainer>
-      <ShaderCanvas height={300} width={300} fragShader={shader} />
+      <ShaderCanvas height={330} width={330} fragShader={shader} />
     </GalleryItemContainer>
   );
 };
