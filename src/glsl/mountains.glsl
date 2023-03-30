6f1e4bb45473e59bb0850169452eeb697bd5231b
Philip Pavo <pavophilip@gmail.com>
2023-03-30 15:41:05 +0200

    mountains

--- src/glsl/mountains.glsl
+++ src/glsl/mountains.glsl
@@ -9,23 +9,22 @@ float random (float x) {
     return fract(sin(x) * 1.0);
 }
 
-float paint(vec2 st, float y){
-    return 1. - smoothstep(y, y + 0.005, st.y);
+float paint(vec2 st, float y, float index){
+    return 1. - smoothstep(y * index / 2., y * index / 2. + 0.005, st.y - index /4.  );
 }
 
 float mountain(float x, float index){
-    float xt = x + u_time / (10. / index) + index;
+    float xt = x + u_time / ( index * 6.) + index * 4.;
 
     float i = floor(xt);
     float f = fract(xt);
 
-    return mix(random(i), random(i + 1.0), smoothstep(0.1, 1., f)) - index;
+    return mix(random(i), random(i + 1.0), smoothstep(0.1, 1., f));
 }
 
 void main(){
     vec2 st = gl_FragCoord.xy / u_resolution;
 
-    vec3 color = vec3(1., 1., 1.);
 
     st *= 4.;
 
@@ -38,11 +37,12 @@ void main(){
     float y2 = mountain(x, 2.);
     float y3 = mountain(x, 3.);
 
-    st.y -= 3.;
-    st.y *= 1.2;
-
     vec4 bg = vec4(vec3(0.945,0.961,0.973), 1.);
-    vec4 mountainColor = vec4(vec3(0.573,0.698,0.8), 1.);
+    vec4 color = vec4(vec3(0.706,0.788,0.859), 1.);
 
-    gl_FragColor = mix(bg, mountainColor, paint(st, y1)) * mix(bg, mountainColor, paint(st, y2)) * mix(bg, mountainColor, paint(st, y3));
+    vec4 m1 = mix(bg, color, paint(st, y1, 1.));
+    vec4 m2 = mix(m1, color, paint(st, y2, 2.));
+    vec4 m3 = mix(m2, color, paint(st, y3, 3.));
+
+    gl_FragColor = m1 * m2 * m3;
 }
